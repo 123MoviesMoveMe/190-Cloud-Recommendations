ad0b977e596ea482fc0f1158837a2bf6d5307284
Matthew Laraia <mlar555@hotmail.com>
2022-11-28 01:18:02 -0500

    now getRecommendation

--- Recommendation Cloud Function/publish.sh
+++ Recommendation Cloud Function/publish.sh
@@ -1,1 +1,2 @@
-gcloud functions deploy get-recommendations --gen2 --region=us-east4 --runtime=python39 --source=. --entry-point=getRecommendations --trigger-http --memory=512 --verbosity=info
+gcloud functions deploy get-recommendations --gen2 --region=us-east4 --runtime=python39 --source=. --entry-point=getRecommendations --trigger-http --verbosity=info
+--memory=512
